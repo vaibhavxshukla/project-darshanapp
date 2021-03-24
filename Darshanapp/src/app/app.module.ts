fe8ebdc3b1ef23b600b90c4e1ca9a8fcc83307e9
vaibhav shukla <vks.vks996@gmail.com>
2021-03-23 22:57:59 -0700

    layout

--- Darshanapp/src/app/app.module.ts
+++ Darshanapp/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { LeftPanelComponent } from './components/layout/left-panel/left-panel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LeftPanelComponent,
   ],
   imports: [
     BrowserModule,
